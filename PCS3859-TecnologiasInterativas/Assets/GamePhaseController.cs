29e53dda0b90b333d21b799aab92338b59ac3a73
Kevin Onishi <kevintaiyo@gmail.com>
2024-11-20 11:42:12 -0300

    Add gameManager and Raycast enabler

---
+++ PCS3859-TecnologiasInterativas/Assets/GamePhaseController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SceneManagement;
+
+public class GamePhaseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject startingPhase;
+    [SerializeField]
+    private GameObject anamnesisPhase;
+    [SerializeField]
+    private GameObject sphygmomanometerPhase;
+    [SerializeField]
+    private GameObject pressureMeasurementPhase;
+    [SerializeField]
+    private XRInteractorLineVisual rightHandRayLine;
+    [SerializeField]
+    private XRRayInteractor rightHandRayEnabler;
+
+    void Start()
+    {
+        startingPhase.SetActive(true);
+        anamnesisPhase.SetActive(false);
+        sphygmomanometerPhase.SetActive(false);
+        pressureMeasurementPhase.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        startingPhase.SetActive(false);
+        anamnesisPhase.SetActive(true);
+    }
+
+    public void SphygmomanometerPlacement()
+    {
+        anamnesisPhase.SetActive(false);
+        sphygmomanometerPhase.SetActive(true) ;
+    }
+    
+    public void PressureMeasurement()
+    {
+        sphygmomanometerPhase.SetActive(false);
+        pressureMeasurementPhase.SetActive(true);
+        rightHandRayEnabler.enabled = true;
+        rightHandRayLine.enabled = true;
+    }
+
+    public void EndGame()
+    {
+        pressureMeasurementPhase.SetActive(false);
+        rightHandRayEnabler.enabled = false;
+        rightHandRayLine.enabled = false;
+        SceneManager.LoadScene("FinalScene_00");
+    }
+}
